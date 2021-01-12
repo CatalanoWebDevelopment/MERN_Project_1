a7ccc460cbfe3cbfe574bf7bd04aa8c9aa38366b
Parker Catalano <catalano.work@gmail.com>
2021-01-12 16:21:18 -0500

    Finished Logout functionality. Moving onto Dashboard."

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { Navbar } from './components/layout/Navbar';
+import Navbar from './components/layout/Navbar';
 import { Landing } from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
