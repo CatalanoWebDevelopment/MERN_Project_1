caff774b00c784a373090b07bc185a73dc9733da
Parker Catalano <catalano.work@gmail.com>
2021-01-14 16:52:20 -0500

    Deleting an account now also removes all related user posts.

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -6,6 +6,7 @@ const auth = require('../../middleware/auth');
 const { check, validationResult } = require('express-validator')
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
+const Post = require('../../models/Post');
 
 // @route   GET api/profile/me
 // @desc    Get current user's profile
@@ -146,6 +147,9 @@ router.get('/user/:user_id', async (req, res) => {
 // @access  Private
 router.delete('/', auth, async (req, res) => {
     try {
+        // Remove User Posts
+        await Post.deleteMany({ user: req.user.id });
+
         // Remove Profile
         await Profile.findOneAndRemove({ user: req.user.id });
 
