3e5ac927625739973a1a916f075a8f3ca3936a00
Parker Catalano <catalano.work@gmail.com>
2021-01-15 17:29:24 -0500

    Added Profile Education, Profile Experience, and Profile Github Repo components.

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -6,6 +6,9 @@ import { getProfileById } from '../../actions/profile';
 import { Link } from 'react-router-dom';
 import ProfileTop from './ProfileTop';
 import ProfileAbout from './ProfileAbout';
+import ProfileExperience from './ProfileExperience';
+import ProfileEducation from './ProfileEducation';
+import ProfileGithub from './ProfileGithub';
 
 const Profile = ({ getProfileById, profile: { profile, loading }, auth, match }) => {
     useEffect(() => {
@@ -23,6 +26,28 @@ const Profile = ({ getProfileById, profile: { profile, loading }, auth, match })
             <div class="profile-grid my-1">
                 <ProfileTop profile={profile} />
                 <ProfileAbout profile={profile} />
+                
+                <div className="profile-exp bg-white p-2">
+                    <h2 class="text-primary">Experience</h2>
+                    {profile.experience.length > 0 ? (<Fragment>
+                        {profile.experience.map((experience) => (
+                            <ProfileExperience key={experience._id} experience={experience} />
+                        ))}
+                    </Fragment>) : (<h4>No Experience Credentials</h4>)}
+                </div>
+
+                <div className="profile-edu bg-white p-2">
+                    <h2 class="text-primary">Education</h2>
+                    {profile.education.length > 0 ? (<Fragment>
+                        {profile.education.map((education) => (
+                            <ProfileEducation key={education._id} education={education} />
+                        ))}
+                    </Fragment>) : (<h4>No Education Credentials</h4>)}
+                </div>
+
+                {profile.githubusername && (
+                    <ProfileGithub username={profile.githubusername} />
+                )}
             </div>
         </Fragment>}
     </Fragment>
