f4f5ef012a29a35f8b99589deb973c9f79ee8946
Parker Catalano <catalano.work@gmail.com>
2021-01-16 11:07:59 -0500

    Fixed spinner and export type.

---
+++ client/src/reducers/post.js
@@ -0,0 +1,33 @@
+/* eslint-disable import/no-anonymous-default-export */
+import {
+    GET_POSTS,
+    POST_ERROR,
+} from '../actions/types';
+
+const initialState = {
+    posts: [],
+    post: null,
+    loading: true,
+    error: {}
+};
+
+export default function(state = initialState, action) {
+    const { type, payload } = action;
+
+    switch (type) {
+        case GET_POSTS:
+            return {
+                ...state,
+                posts: payload,
+                loading: false
+            };
+        case POST_ERROR: 
+            return {
+                ...state,
+                posts: payload,
+                loading: false
+            };
+        default:
+            return state;
+    }
+}
