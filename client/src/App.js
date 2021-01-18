68cb712a334192e18a2c8e5c4072e39ed102b29e
Parker Catalano <catalano.work@gmail.com>
2021-01-18 17:57:56 -0500

    Save

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,7 @@ import AddExperience from './components/profile-forms/AddExperience';
 import AddEducation from './components/profile-forms/AddEducation';
 import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
+import Posts from './components/posts/Posts';
 import PrivateRoute from './components/routing/PrivateRoute';
 
 // Redux
@@ -36,19 +37,20 @@ const App = () => {
       <Router>
         <Fragment>
           <Navbar />
-          <Route exact path='/' component={ Landing } />
+          <Route exact path='/' component={Landing} />
           <section className="container">
             <Alert />
             <Switch>
-              <Route exact path='/register' component={ Register } />
-              <Route exact path='/login' component={ Login } />
-              <Route exact path='/profiles' component={ Profiles } />
-              <Route exact path='/profile/:id' component={ Profile } />
-              <PrivateRoute exact path='/dashboard' component={ Dashboard } />
-              <PrivateRoute exact path='/create-profile' component={ CreateProfile } />
-              <PrivateRoute exact path='/edit-profile' component={ EditProfile } />
-              <PrivateRoute exact path='/add-experience' component={ AddExperience } />
-              <PrivateRoute exact path='/add-education' component={ AddEducation } />
+              <Route exact path='/register' component={Register} />
+              <Route exact path='/login' component={Login} />
+              <Route exact path='/profiles' component={Profiles} />
+              <Route exact path='/profile/:id' component={Profile} />
+              <PrivateRoute exact path='/dashboard' component={Dashboard} />
+              <PrivateRoute exact path='/create-profile' component={CreateProfile} />
+              <PrivateRoute exact path='/edit-profile' component={EditProfile} />
+              <PrivateRoute exact path='/add-experience' component={AddExperience} />
+              <PrivateRoute exact path='/add-education' component={AddEducation} />
+              <PrivateRoute exact path='/posts' component={Posts} />
             </Switch>
           </section>
         </Fragment>
