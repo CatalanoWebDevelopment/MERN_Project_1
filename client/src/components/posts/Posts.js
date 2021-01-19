4b3d975004091bd14bab768f356d06632495b291
Parker Catalano <catalano.work@gmail.com>
2021-01-19 15:18:52 -0500

    Fixed authentication issue.

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -2,18 +2,34 @@ import React, { Fragment, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { getPosts } from '../../actions/post';
+import { loadUser } from '../../actions/auth';
 import Spinner from '../layout/Spinner';
+import PostItem from './PostItem';
 
-const Posts = ({ getPosts, post: { posts, loading } }) => {
+const Posts = ({ getPosts, loadUser, post: { posts, loading } }) => {
     useEffect(() => {
+        loadUser();
         getPosts();
-    }, [getPosts]);
+    }, [getPosts, loadUser]);
 
-    return <div />
+    return loading ? <Spinner /> : <Fragment>
+        <h1 className="large text-primary">Posts</h1>
+        <p className="lead">
+            <i className="fas fa-user"></i> Welcome to the community
+        </p>
+
+        {/* PostForm */}
+        <div className="posts">
+            {posts.map(post => (
+                <PostItem key={post._id} post={post} />
+            ))}
+        </div>
+    </Fragment>;
 };
 
 Posts.propTypes = {
     getPosts: PropTypes.func.isRequired,
+    loadUser: PropTypes.func.isRequired,
     post: PropTypes.object.isRequired,
 };
 
@@ -21,4 +37,4 @@ const mapStateToProps = state => ({
     post: state.post
 });
 
-export default connect(mapStateToProps, { getPosts })(Posts);
+export default connect(mapStateToProps, { loadUser, getPosts })(Posts);
