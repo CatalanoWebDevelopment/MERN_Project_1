b27d475d1b69919c3b953c23099f0dbaeeb17a5f
Parker Catalano <catalano.work@gmail.com>
2021-01-21 14:54:49 -0500

    Users can now delete a post that they have created.

--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -2,7 +2,8 @@
 import {
     GET_POSTS,
     POST_ERROR,
-    UPDATE_LIKES
+    UPDATE_LIKES,
+    DELTE_POST
 } from '../actions/types';
 
 const initialState = {
@@ -20,6 +21,12 @@ export default function(state = initialState, action) {
             return {
                 ...state,
                 posts: payload,
+                loading: false
+            };
+        case DELTE_POST:
+            return {
+                ...state,
+                posts: state.posts.filter(post => post._id !== payload),
                 loading: false
             };
         case POST_ERROR: 
